b60aaf9c41dc02ea02a557243e7bf76226198ca0
Ruhan Shaikh <ruhan1192002@gmail.com>
2025-12-15 15:12:27 +0530

    bg color added

--- 4) JS/1.2) Random-Card-Generator/script.js
+++ 4) JS/1.2) Random-Card-Generator/script.js
@@ -1,30 +1,34 @@
-//  <i class="ri-trophy-fill"></i>
-let teams=[
-    {
-      captain:"url(asset/dhoni.jpg)",
-      name:"Mahendra Singh Dhoni",
-      logo:"asset/chennai-logo.jpg",
-      trophy:"5"
-    },
-    {
-      captain:"url(asset/kohli.jpg)",
-      name:"Virat Kohli",
-      logo:"asset/rcb-logo.jpg",
-      trophy:"1"
-    },
-    {
-      captain:"url(asset/rohit.jpg)",
-      name:"Rohit Sharma",
-      logo:"asset/mumbai-logo.jpg",
-      trophy:"5"
-    },
-    {
-      captain:"url(asset/gautum.jpg)",
-      name:"Gautam Gambhir",
-      logo:"asset/kkr-logo.jpg",
-      trophy:"2"
-    },
+let teams = [
+  {
+    captain: "asset/dhoni.jpg",
+    name: "Mahendra Singh Dhoni",
+    logo: "asset/chennai-logo.jpg",
+    trophy: 5,
+    primary: "#ffcc00",     // CSK Yellow
+  },
+  {
+    captain: "asset/kohli.jpg",
+    name: "Virat Kohli",
+    logo: "asset/rcb-logo.jpg",
+    trophy: 1,
+    primary: "#ff0000",     // RCB Red
+  },
+  {
+    captain: "asset/rohit.jpg",
+    name: "Rohit Sharma",
+    logo: "asset/mumbai-logo.jpg",
+    trophy: 5,
+    primary: "#004c97",     // MI Blue
+  },
+  {
+    captain: "asset/gautum.jpg",
+    name: "Gautam Gambhir",
+    logo: "asset/kkr-logo.jpg",
+    trophy: 2,
+    primary: "#3a0ca3",     // KKR Purple
+  }
 ];
+
 let btn=document.querySelector('button');
 let card=document.querySelector('.card');
 let item1=document.querySelector('.item1');
@@ -34,7 +38,7 @@ let item3=document.querySelector('.item3');
 let item4=document.querySelector('.item4');
 btn.addEventListener('click',()=>{
     let random=Math.floor(Math.random()*teams.length);
-    item1.style.backgroundImage=teams[random].captain;
+    item1.style.backgroundImage=`url(${teams[random].captain})`;
     h1.innerHTML=teams[random].name;
     item3.style.backgroundImage=`url(${teams[random].logo})`;
     let icons="";
@@ -43,4 +47,7 @@ btn.addEventListener('click',()=>{
     }
     item4.innerHTML=icons;
     card.style.backgroundImage="none";
+    item2.style.backgroundColor=teams[random].primary;
+    card.style.backgroundImage="none";
+    item4.style.backgroundColor=teams[random].primary;
 })
