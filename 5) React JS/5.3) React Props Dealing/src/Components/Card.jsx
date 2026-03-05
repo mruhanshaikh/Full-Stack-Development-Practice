2f0e38c9f99c13156e6fdd16aa76191acc5a8a33
Ruhan Shaikh <ruhan1192002@gmail.com>
2026-03-05 19:07:44 +0530

    tailwindcss

--- 5) React JS/5.3) React Props Dealing/src/Components/Card.jsx
+++ 5) React JS/5.3) React Props Dealing/src/Components/Card.jsx
@@ -1,7 +1,9 @@
 const Card = () => {
   return (
-    <div className="bg-white w-1">
-        
+    <div className="card">
+      <div className="flex"><div className="rounded-full  bg-gray-300"><span>🤖</span></div><div className="flex"><span>Save</span><span>⬆️</span></div></div>
+      <div></div>
+      <div></div>
     </div>
   )
 }
