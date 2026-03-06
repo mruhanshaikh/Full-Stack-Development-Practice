ea404f9a574801ed2aff704af13b36f7bf7ac084
Ruhan Shaikh <ruhan1192002@gmail.com>
2026-03-06 19:23:29 +0530

    react props and tailwind done

--- 5) React JS/5.3) React Props Dealing/src/App.jsx
+++ 5) React JS/5.3) React Props Dealing/src/App.jsx
@@ -1,8 +1,78 @@
 import Card from "./Components/Card"
+import { RiAmazonFill } from "@remixicon/react";
+import { RiGoogleFill } from "@remixicon/react";
+import { RiDribbbleFill } from "@remixicon/react";
+import { RiFigmaFill } from "@remixicon/react";
+import { RiAppleFill } from "@remixicon/react";
+import { RiMicrosoftFill } from "@remixicon/react";
 const App = () => {
+  const jobs = [
+  {
+    logo:<RiAmazonFill size={36} />,
+    company: "Amazon",
+    postedDaysAgo: 5,
+    isBookmarked: false,
+    position: "AWS Practitioner",
+    tags: ["Part-Time", "Senior Level"],
+    salary: "$120/hr",
+    location: "Mumbai, India"
+  },
+  {
+    logo:<RiGoogleFill size={36} />,
+    company: "Google",
+    postedDaysAgo: 30,
+    isBookmarked: true,
+    position: "Software Engineer",
+    tags: ["Part-Time", "Flexible Schedule"],
+    salary: "$150–220k",
+    location: "Kochi, India"
+  },
+  {
+    logo:<RiDribbbleFill size={36} />,
+    company: "Dribbble",
+    postedDaysAgo: 18,
+    isBookmarked: false,
+    position: "Senior Motion Designer",
+    tags: ["Contract", "Remote"],
+    salary: "$85/hr",
+    location: "Chennai, India"
+  },
+  {
+    logo:<RiFigmaFill size={36}/>,
+    company: "Figma",
+    postedDaysAgo: 15,
+    isBookmarked: true,
+    position: "Full Stack Developer",
+    tags: ["Full-Time", "In office"],
+    salary: "$200–250k",
+    location: "Bangalore, India"
+  },
+  {
+    logo:<RiMicrosoftFill size={36}/>,
+    company: "Microsoft",
+    postedDaysAgo: 3,
+    isBookmarked: false,
+    position: "Junior UI/UX Designer",
+    tags: ["Contract", "Remote"],
+    salary: "$100/hr",
+    location: "Delhi, India"
+  },
+  {
+    logo:<RiAppleFill size={36}/>,
+    company: "Apple",
+    postedDaysAgo: 25,
+    isBookmarked: true,
+    position: "Product Manager",
+    tags: ["Full-Time", "Flexible Schedule"],
+    salary: "$85–120k",
+    location: "Kerala, India"
+  }
+];
   return (
-    <div>
-      <Card/>
+    <div className="flex flex-wrap justify-center gap-[1rem] p-[1rem]">
+      {jobs.map(e=>{
+        return <Card logo={e.logo} bookmark={e.isBookmarked} company={e.company} position={e.position} salary={e.salary} location={e.location} postedDaysAgo={e.postedDaysAgo} tag0={e.tags[0]} tag1={e.tags[1]}/>
+      })}
     </div>
   )
 }
--- 5) React JS/5.3) React Props Dealing/src/Components/Card.jsx
+++ 5) React JS/5.3) React Props Dealing/src/Components/Card.jsx
@@ -1,10 +1,55 @@
-const Card = () => {
+import { RiBookmarkLine } from "@remixicon/react";
+import { RiBookmarkFill } from "@remixicon/react";
+
+const Card = (details) => {
   return (
     <div className="card">
-      <div className="flex"><div className="rounded-full  bg-gray-300"><span>🤖</span></div><div className="flex"><span>Save</span><span>⬆️</span></div></div>
-      <div></div>
-      <div></div>
+      {/* FIRST ROW */}
+      <div className="flex  justify-between w-full">
+        <div>
+          <div className="w-[50px] flex justify-center items-center border-2 border-solid border-gray-300 h-[50px] rounded-full">
+            <span className="flex items-center">
+              {details.logo}
+            </span>
+          </div>
+        </div>
+        <div>
+          <div className="flex  group items-center outline-1 outline-gray-300 py-0.5 px-2 hover:bg-gray-300 transition duration-300 ease-in rounded-lg">
+            <span className="text-gray-500">Save</span>
+            <span className="group-hover:text-gray-50 cursor-pointer">
+              {details.bookmark?<RiBookmarkLine size={18} color="gray" />:<RiBookmarkFill size={18} color="gray" />}
+            </span>
+          </div>
+        </div>
+      </div>
+      {/* SECOND ROW */}
+      <div>
+        <div className="flex gap-1 items-center">
+          <span className="font-[600] text-[18px] capitalize">{details.company}</span>
+          <span className="text-[12px] text-gray-500">{details.postedDaysAgo} days ago</span>
+        </div>
+        <div className="font-[700] text-[20px] capitalize">{details.position}</div>
+        <div className="mt-2 font-[500] text-[12px] text-gray-600 flex gap-[1rem] items-center">
+          <span className=" bg-gray-300 px-3 py-1 rounded-lg hover:bg-gray-200 transition duration-300 ease-out cursor-pointer capitalize">{details.tag0}</span>
+          <span className=" transition duration-300 ease-out  hover:bg-gray-200 bg-gray-300 px-3 py-1 rounded-lg cursor-pointer capitalize">
+            {details.tag1}
+          </span>
+        </div>
+      </div>
+      {/* THIRD ROW */}
+      <div className="flex flex-col gap-[1.5rem]">
+        <div className="border-t border-t-gray-200"></div>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col">
+            <span className="text-[18px] font-bold">{details.salary}</span>
+            <span className="text-[12px] text-gray-500 capitalize">{details.location}</span>
+          </div>
+          <button className="hover:scale-[1.1] transition duration-300 ease-in hover:bg-black/60 bg-black text-[14px] py-1.5 px-3 rounded-lg text-white cursor-pointer border-none capitalize">
+            Apply now 
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
-export default Card
+  );
+};
+export default Card;
